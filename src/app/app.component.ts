d5bff8fbeac09befe11af90333f332bf169150b8
androowl <kmgod65@cheroh.com>
2021-12-22 14:02:34 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { BehaviorSubject, catchError, Observable, of, startWith } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AppState } from './enum/app-state';
 import { DataState } from './enum/data-state.enum';
 import { Status } from './enum/status.enum';
 import { CustomResponse } from './interface/custom.response';
+import { Server } from './interface/server';
 import { ServerService } from './service/server.service';
 
 @Component({
@@ -19,6 +21,8 @@ export class AppComponent implements OnInit {
   private filterSubject = new BehaviorSubject<string>('');
   private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoading.asObservable();
 
   constructor(private serverService: ServerService) { }
 
@@ -27,7 +31,7 @@ export class AppComponent implements OnInit {
       .pipe(
         map(response => {
           this.dataSubject.next(response);
-          return { dataState: DataState.LOADED_STATE, appData: response }
+          return { dataState: DataState.LOADED_STATE, appData: { ...response, data: { servers: response.data.servers.reverse()} } }
         }),
         startWith({ dataState: DataState.LOADING_STATE }),
         catchError((error: string) => {
@@ -38,12 +42,12 @@ export class AppComponent implements OnInit {
 
   pingServer(ipAddress: string): void {
     this.filterSubject.next(ipAddress);
-    this.appState$ = this.serverService.servers$
+    this.appState$ = this.serverService.ping$(ipAddress)
       .pipe(
         map(response => {
           const index = this.dataSubject.value.data.servers.findIndex(server => server.id === response.data.server.id);
           this.dataSubject.value.data.servers[index] = response.data.server
-          this.filterSubject.next(' ');
+          this.filterSubject.next('');
           return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
         }),
         startWith({ dataState: DataState.LOADING_STATE, appData: this.dataSubject.value }),
@@ -52,5 +56,69 @@ export class AppComponent implements OnInit {
           return of({ dataState: DataState.ERROR_STATE, error })
         })
       );
+  }
+
+  filterServers(status: Status): void {
+    this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
+  }
+
+  saveServer(serverForm: NgForm): void {
+    this.isLoading.next(true);
+    this.appState$ = this.serverService.save$(serverForm.value as Server)
+      .pipe(
+        map(response => {
+          this.dataSubject.next(
+            {...response, data: { servers: [response.data.server,...this.dataSubject.value.data.servers] } }
+          );
+          document.getElementById('closeModel').click();
+          this.isLoading.next(false);
+          serverForm.resetForm({ status: this.Status.SERVER_DOWN })
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.isLoading.next(false);
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
+  }
+
+  deleteServer(server: Server): void {
+    this.appState$ = this.serverService.delete$(server.id)
+      .pipe(
+        map(response => {
+          this.dataSubject.next(
+            { ...response, data: { servers: this.dataSubject.value.data.servers.filter(s => s.id !== server.id)} }
+          );
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADING_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.filterSubject.next('');
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
+  }
+
+  printReport(): void {
+    //window.print();
+    let dataType = 'application/vnd.ms-excel.sheet.macroEnabled.12';
+    let tableSelect = document.getElementById('servers');
+    let tableHtml = tableSelect.outerHTML.replace(/ /g, '%20');
+    let downloadLink = document.createElement('a');
+    document.body.appendChild(downloadLink);
+    downloadLink.href = 'data:' + dataType + ', ' + tableHtml;
+    downloadLink.download = 'server-report.xls';
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   }
 }
